311d1c8de23c738ffc3684c14623de8ed8e1b85a
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-09-03 22:44:11 +0300

    nine

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { IsString, Length } from "class-validator";
+import { IsEmail, IsInt, IsNotIn, IsNumber, IsOptional, IsOptional, IsString, Length, Matches, Max, Min } from "class-validator";
 
 export class CreateUserDto{
 
@@ -6,6 +6,27 @@ export class CreateUserDto{
   @Length(2,20)
   public readonly name: string;
 
-  
+  @IsString()
+  @IsEmail()
+  public readonly email: string;
+
+  @IsNotIn(['password', 'qwe', '123'])
+  @IsString()
+  @Matches(/^[a-z0-9]+$/, {message: 'Invalid email address'})
+  public readonly password: string;
+
+
+  @IsInt()
+  @IsNumber()
+  @Max(120)
+  @Min(15)
+  @IsOptional()
+  public readonly age?: number;
+
+
+  @IsString()
+  @IsOptional()
+  public readonly phone?: string;
+
 }
 
