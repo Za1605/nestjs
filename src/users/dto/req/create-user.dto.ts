60ffdf5e1a4645f5ef9c5c29bb8b796aab8d7ce8
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-09-04 21:52:40 +0300

    ten

--- src/users/dto/req/create-user.dto.ts
+++ src/users/dto/req/create-user.dto.ts
@@ -1,20 +1,34 @@
-import { ApiProperty } from "@nestjs/swagger";
+
+import { IsEmail, IsInt, IsNotIn, IsNumber, IsOptional, IsString, Length, Matches, Max, Min } from "class-validator";
 
 export class CreateUserDto {
 
 
+  @IsString()
+  @Length(2,20)
+  public readonly name: string;
+
+  @IsString()
+  @IsEmail()
   public readonly email: string;
 
-
+  @IsNotIn(['password', 'qwe', '123'])
+  @IsString()
+  @Matches(/^[a-z0-9]+$/, {message: 'Invalid email address'})
   public readonly password: string;
 
 
-  public readonly name: string;
-
-
-  public readonly phone?: string;
-
-
+  @IsInt()
+  @IsNumber()
+  @Max(120)
+  @Min(15)
+  @IsOptional()
   public readonly age?: number;
 
+
+  @IsString()
+  @IsOptional()
+  public readonly phone?: string;
+
 }
+
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+
 import { PostsModule } from 'src/posts/dto/res/posts.module';
 
 
 @Module({
   imports:[PostsModule],
-  controllers: [UsersController],
+  //controllers: [UsersController],
   providers: [UsersService],
 
 
